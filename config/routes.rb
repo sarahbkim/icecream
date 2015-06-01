1b7a29fb578fbecc975372e279cb96e9a0e13797
sarahbkim <pnkdaegee@gmail.com>
2015-05-31 18:28:20 -0700

    add users resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
-  get 'signup' => 'users#new'
+  resources :users
+  match '/signup', to: 'users#new', via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
