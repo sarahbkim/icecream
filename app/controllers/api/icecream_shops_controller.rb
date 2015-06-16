bf5eedb404b21d0bc782676f1321c2b60c46a733
sarahbkim <pnkdaegee@gmail.com>
2015-06-15 22:25:58 -0700

    fix

--- app/controllers/api/icecream_shops_controller.rb
+++ app/controllers/api/icecream_shops_controller.rb
@@ -17,13 +17,14 @@ class Api::IcecreamShopsController < ApplicationController
   def search
     # search in app's database first
     results = IcecreamShop.where('city like :location_q or name like :name_q', location_q: params[:location], name_q: params[:store_name] ).to_a
-    
+    print (results.empty?)
     # if no results, call in Factual api
     if results.empty?
       args_hash = { store_name: params[:store_name], location: params[:location] }
       results = GetFactualData.call(args_hash)
-      if result.success?
-        return result.results_arr
+      print results.results_arr
+      if results.success?
+        return results.results_arr
       end
     else
       return results
