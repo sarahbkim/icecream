c11448856b26ac3d46cfcfb96136da286ed00f97
sarahbkim <pnkdaegee@gmail.com>
2015-06-21 17:05:53 -0700

    update api

--- app/assets/javascripts/icecream.js
+++ app/assets/javascripts/icecream.js
@@ -1,11 +1,28 @@
 'use strict';
-angular.module('icecreamApp', ['ngResource'])
+
+console.log("this is the icecream.js page");
+var app = angular.module('icecreamApp', ['ngResource']);
+
+app.factory('iceCreamData', ['$resource', function($resource){
+
+    return $resource('/api/icecream_shops/', {}, {
+      query: {method: 'GET', params: {}, isArray: false}
+
+    });
+  }])
+  .factory('checkInsData', ['$resource', function($resource){
+
+    return $resource('/api/check_ins/', {}, {
+      query: {method: 'GET', params: {}, isArray: false}
+
+    })
+  }])
   .controller('MainCtrl', ['$scope', 'iceCreamData', function($scope, iceCreamData){
+    console.log('mainctrl');
 
     $scope.findStores = function(store_name, location_query) {
       var store_name = store_name || "",
           location_query = location_query || "";
-
       $scope.data = iceCreamData.query({store_name: store_name, location: location_query});
       $scope.data.$promise.then(function(d){
         return d;
@@ -15,23 +32,10 @@ angular.module('icecreamApp', ['ngResource'])
   }])
   .controller('StatsCtrl', ['$scope', 'checkInsData', function($scope, checkInsData){
 
-    $scope.checkIns = StatsCtrl.query({user_id: user_id});
+    $scope.checkIns = checkInsData.query();
     $scope.checkIns.$promise.then(function(d){
-      return d;
+      console.log(d.check_ins);
+      return d.check_ins;
     });
 
-  }])
-  .factory('iceCreamData', ['$resource', function($resource){
-
-    return $resource('/api/icecream_shops/', {}, {
-      query: {method: 'GET', params: {}, isArray: false}
-
-    });
-  }])
-  .factory('checkInsData', ['$resource', function($resource){
-
-    return $resource('/api/favorite_shops/', {}, {
-      query: {method: 'GET', params: {}, isArray: false}
-
-    })
   }]);
--- app/controllers/api/check_ins_controller.rb
+++ app/controllers/api/check_ins_controller.rb
@@ -2,8 +2,22 @@ class Api::CheckInsController < ApplicationController
   #before_action :authenticate
 
   def index
-    @check_ins = CheckIn.where(user_id: current_user.id).to_a
-    render json: @check_ins, status: 201
+    check_ins = CheckIn.joins("join icecream_shops on check_ins.shop_id = icecream_shops.id")
+      .where(user_id: current_user.id)
+      .select("check_ins.id, check_ins.created_at, icecream_shops.name AS shop_name")
+
+    # group the json by
+
+    uniq_check_in_dates = check_ins.group_by{ |c| c.created_at.to_date }
+
+    @check_in_count = []
+
+    uniq_check_in_dates.each do |date, stores|
+      count = stores.length
+      @check_in_count << {date: date, count: count, stores: stores }
+    end
+
+    render json: @check_in_count, status: 201
   end
 
 end
